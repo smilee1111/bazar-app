1eba9d0b5244001dacf0c429901c911c43a2c2a9
smilee1111 <muskankc2005@gmail.com>
2026-01-02 17:12:23 +0545

    state and viewmodel for auth

---
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:bazar/core/error/failure.dart';
+import 'package:bazar/core/usecases/app_usecase.dart';
+import 'package:bazar/features/auth/data/repositories/auth_repository.dart';
+import 'package:bazar/features/auth/domain/repositories/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Create Provider
+final logoutUsecaseProvider = Provider<LogoutUsecase>((ref) {
+  final authRepository = ref.read(authRepositoryProvider);
+  return LogoutUsecase(authRepository: authRepository);
+});
+
+class LogoutUsecase implements UsecaseWithoutParams<bool> {
+  final IAuthRepository _authRepository;
+
+  LogoutUsecase({required IAuthRepository authRepository})
+    : _authRepository = authRepository;
+
+  @override
+  Future<Either<Failure, bool>> call() {
+    return _authRepository.logout();
+  }
+}
